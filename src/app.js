34222b49768db0aa878f0794335f950e9b76c588
Klaus Nielsen <klnie1989@gmail.com>
2019-10-14 21:18:37 +0200

    Removing hbs support from server script

--- src/app.js
+++ src/app.js
@@ -1,44 +1,18 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Path definitions for express confirguration
-const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
-
-//set express to use handlebars templating engine
-app.set('view engine', 'hbs');
-//set the template folder dir for hbs views
-app.set('views', viewsPath);
-//set the partials folder for hbs to use
-hbs.registerPartials(partialsPath);
+const publicDirPath = path.join(__dirname, '../dist');
 
 //express static route to main dir
 app.use(express.static(publicDirPath));
 
-//get request to route handlebars page
 app.get('', (req, res) => {
-    //res.render page filename and then object data
-    res.render('index', {
-        title: 'STL Estimator'
-    });
-});
-
-app.get('/contributors', (req, res) => {
-    res.render('contributors', {
-        title: 'STL Estimator Contributors'
-    });
-});
-
-app.get('/style-guide', (req, res) => {
-    res.render('style-guide', {
-        title: 'STL Estimator Style'
-    });
+    res.sendFile(path.resolve(publicDirPath, 'index.html'))
 });
 
 app.listen(port, () => {
